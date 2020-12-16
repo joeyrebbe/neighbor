e31e03845f6c9f2cef5654bc319ef97278d9504d
Aleksei Yarantsev <aleksyara@Alekseis-MacBook-Pro.local>
2020-12-15 20:08:49 -0800

    implement login, signup, create jobpost

--- neighbor/main_app/models.py
+++ neighbor/main_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+import datetime
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
+
+class JobPost(models.Model):
+    name = models.CharField(max_length = 100)
+    description = models.TextField(max_length = 250)
+    date = models.DateField(("Date"), default=datetime.date.today)
+    maxPeople = models.IntegerField()
+    compensation = models.TextField(max_length = 250)
+    # Add the foreign key linking to a user instance
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        # return reverse('index', kwargs={'job_post_id': self.id})
+        return reverse('index')
+
+  
--- neighbor/neighbor/settings.py
+++ neighbor/neighbor/settings.py
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Add this variable to specify where successful logins should redirect to
+LOGIN_REDIRECT_URL = '/'
+
+LOGOUT_REDIRECT_URL = '/'
