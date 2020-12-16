d1115b6ef6284dd4b7645fbd599e95bd950c25f7
Aleksei Yarantsev <aleksyara@Alekseis-MacBook-Pro.local>
2020-12-15 17:25:58 -0800

    work in progress

--- neighbor/main_app/views.py
+++ neighbor/main_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+  return render(request, 'home.html') 
+
+  # Define the ABOUT/ view
+# def about(request):
+#   return HttpResponse('<h1>This is About Page</h1>') # simmiluar to res.send
+
+def about(request):
+    return render(request, 'about.html') #instead of HTTPResponse we can use a template
