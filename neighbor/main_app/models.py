ed4f1de5d52eb0286524c965d026b82b0466fe0d
Aleksei Yarantsev <aleksyara@Alekseis-MacBook-Pro.local>
2020-12-16 14:49:47 -0800

    update indention

--- neighbor/main_app/models.py
+++ neighbor/main_app/models.py
@@ -34,7 +34,7 @@ class Volonteer(models.Model):
   name = models.CharField(max_length=50)
   
   def __str__(self):
-    return self.name
+      return self.name
 
   def get_absolute_url(self):
       return reverse('volonteer_detail', kwargs={'pk': self.id})
